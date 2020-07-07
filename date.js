ad5bfabf3dc0ad6a70be78a5802cd0e619667e1b
Abner Pena de Souza <abner.souza@crm.gov.br>
2020-07-07 16:42:19 -0300

    Inserts the file responsible to learn about date calculation

---
+++ date.js
@@ -0,0 +1,20 @@
+// Arquivo para conversão da data em idade
+
+// Exemplo: 1573568256893
+
+function age(timestamp){
+    // Criando novo objeto de data e inserindo na variável "today"
+    const today = new Date()
+    const birthDate = new Date(timestamp)
+
+    // 2020 - 1984 = 35
+    let age = today.getFullYear() - birthDate.getFullYear()
+    const month = today.getMonth() - birthDate.getMonth()
+
+    if (month < 0 || month == 0){
+        age = age -1
+    }
+
+    return age
+
+}
