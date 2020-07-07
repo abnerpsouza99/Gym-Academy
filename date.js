6f637e4772408ecd628735f8c0e4ce3955e19e7a
Abner Pena de Souza <abner.souza@crm.gov.br>
2020-07-07 16:48:29 -0300

    Update the date file

--- date.js
+++ date.js
@@ -11,10 +11,8 @@ function age(timestamp){
     let age = today.getFullYear() - birthDate.getFullYear()
     const month = today.getMonth() - birthDate.getMonth()
 
-    if (month < 0 || month == 0){
+    if (month < 0 || month == 0 && today.getDate() < birthDate.getDate()){
         age = age -1
     }
-
     return age
-
 }
